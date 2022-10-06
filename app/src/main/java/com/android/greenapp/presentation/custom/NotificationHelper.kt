780ed254523b52930df7415319f5d78a0a662034
user <mail@mail.com>
2022-10-06 10:13:29 +0600

    done with name app

--- app/src/main/java/com/android/greenapp/presentation/custom/NotificationHelper.kt
+++ app/src/main/java/com/android/greenapp/presentation/custom/NotificationHelper.kt
@@ -19,56 +19,56 @@ import javax.inject.Inject
 @AppScope
 class NotificationHelper @Inject constructor(private val applicationContext: Context) {
 
-    private val greenAppChannel = "Green App Messages"
+	private val greenAppChannel = "Green App Messages"
 
-    fun callGreenAppNotificationMessages(message: String, create_at_time: Long) {
-        buildingNotificationChannels()
-        VLog.d("Notification got called with message : $message and time : $create_at_time")
-        val title = "Green App Message"
+	fun callGreenAppNotificationMessages(message: String, create_at_time: Long) {
+		buildingNotificationChannels()
+		VLog.d("Notification got called with message : $message and time : $create_at_time")
+		val title = "Green Wallet"
 
-        val notifIntent = Intent(applicationContext, IntroActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            action=Intent.ACTION_MAIN
-            addCategory(Intent.CATEGORY_LAUNCHER)
-        }
+		val notifIntent = Intent(applicationContext, IntroActivity::class.java).apply {
+			flags = Intent.FLAG_ACTIVITY_NEW_TASK
+			action = Intent.ACTION_MAIN
+			addCategory(Intent.CATEGORY_LAUNCHER)
+		}
 
-        val pendingIntent = PendingIntent.getActivity(
-            applicationContext,
-            1,
-            notifIntent,
-            PendingIntent.FLAG_IMMUTABLE
-        )
+		val pendingIntent = PendingIntent.getActivity(
+			applicationContext,
+			1,
+			notifIntent,
+			PendingIntent.FLAG_IMMUTABLE
+		)
 
-        val builder = NotificationCompat.Builder(applicationContext, greenAppChannel)
-            .setSmallIcon(R.drawable.ic_chia_white)
-            .setContentTitle(title)
-            .setContentText(message)
-            .setAutoCancel(true)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setContentIntent(pendingIntent)
+		val builder = NotificationCompat.Builder(applicationContext, greenAppChannel)
+			.setSmallIcon(R.drawable.ic_chia_white)
+			.setContentTitle(title)
+			.setContentText(message)
+			.setAutoCancel(true)
+			.setPriority(NotificationCompat.PRIORITY_DEFAULT)
+			.setContentIntent(pendingIntent)
 
-        val notificationManager = NotificationManagerCompat.from(applicationContext)
-        notificationManager.notify(101, builder.build())
-    }
+		val notificationManager = NotificationManagerCompat.from(applicationContext)
+		notificationManager.notify(101, builder.build())
+	}
 
 
-    fun buildingNotificationChannels() {
+	fun buildingNotificationChannels() {
 
-        val channels = listOf(greenAppChannel)
-        channels.forEach {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                val importance: Int = NotificationManager.IMPORTANCE_DEFAULT
-                val channel = NotificationChannel(
-                    it,
-                    it,
-                    importance
-                )
-                channel.lightColor = Color.MAGENTA
-                val notifManager = applicationContext.applicationContext
-                    .getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-                notifManager.createNotificationChannel(channel)
-            }
-        }
-    }
+		val channels = listOf(greenAppChannel)
+		channels.forEach {
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+				val importance: Int = NotificationManager.IMPORTANCE_DEFAULT
+				val channel = NotificationChannel(
+					it,
+					it,
+					importance
+				)
+				channel.lightColor = Color.MAGENTA
+				val notifManager = applicationContext.applicationContext
+					.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+				notifManager.createNotificationChannel(channel)
+			}
+		}
+	}
 
 }
