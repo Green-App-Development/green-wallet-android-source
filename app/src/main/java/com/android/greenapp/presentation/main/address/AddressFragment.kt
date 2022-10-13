487a308635236e1fcbbbfa90f9d95f1d14b303b8
user <mail@mail.com>
2022-10-13 10:51:04 +0600

    done with animating address items

--- app/src/main/java/com/android/greenapp/presentation/main/address/AddressFragment.kt
+++ app/src/main/java/com/android/greenapp/presentation/main/address/AddressFragment.kt
@@ -21,6 +21,7 @@ import com.android.greenapp.presentation.viewBinding
 import com.example.common.tools.VLog
 import com.android.greenapp.presentation.tools.getStringResource
 import dagger.android.support.DaggerFragment
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
 import javax.inject.Inject
 
@@ -31,137 +32,136 @@ import javax.inject.Inject
  */
 class AddressFragment : DaggerFragment(), AddressAdapter.EditedOpenListener {
 
-    private val binding by viewBinding(FragmentAddressListBinding::bind)
+	private val binding by viewBinding(FragmentAddressListBinding::bind)
 
-    @Inject
-    lateinit var anim: AnimationManager
+	@Inject
+	lateinit var anim: AnimationManager
 
-    @Inject
-    lateinit var dialogManager: DialogManager
+	@Inject
+	lateinit var dialogManager: DialogManager
 
-    private var addressAdapter: AddressAdapter? = null
+	private var addressAdapter: AddressAdapter? = null
 
-    private var shouldBeClickable: Boolean = false
+	private var shouldBeClickable: Boolean = false
 
-    @Inject
-    lateinit var viewModelFactory: ViewModelFactory
-    private val addressViewModel: AddressViewModel by viewModels { viewModelFactory }
+	@Inject
+	lateinit var viewModelFactory: ViewModelFactory
+	private val addressViewModel: AddressViewModel by viewModels { viewModelFactory }
 
-    companion object {
-        const val SHOULD_BE_CLICKABLE_KEY = "should_be_clickable"
-    }
+	companion object {
+		const val SHOULD_BE_CLICKABLE_KEY = "should_be_clickable"
+	}
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            shouldBeClickable = it.getBoolean(SHOULD_BE_CLICKABLE_KEY, false)
-        }
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		arguments?.let {
+			shouldBeClickable = it.getBoolean(SHOULD_BE_CLICKABLE_KEY, false)
+		}
+	}
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return layoutInflater.inflate(R.layout.fragment_address_list, container, false)
-    }
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		return layoutInflater.inflate(R.layout.fragment_address_list, container, false)
+	}
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        registerClicks()
-        initRecAddressView()
-        searchFunctionality()
-    }
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		registerClicks()
+		initRecAddressView()
+		searchFunctionality()
+	}
 
-    private fun searchFunctionality() {
-        binding.edtSearchAddress.addTextChangedListener {
-            if (it == null) return@addTextChangedListener
-            addressViewModel.searchAddressByName(name = it.toString().trim())
-        }
-    }
-
-
-    private fun initRecAddressView() {
-        addressAdapter = AddressAdapter(this, anim = anim, shouldBeClickable)
-        binding.recViewAddresses.apply {
-            layoutManager = LinearLayoutManager(curActivity())
-            adapter = addressAdapter
-        }
-        lifecycleScope.launchWhenStarted {
-            addressViewModel.address_items.collect {
-                it?.let {
-                    VLog.d("Getting List of Addresses : $it")
-                    addressAdapter!!.updateAddressList(it)
-                }
-            }
-        }
-    }
-
-    private fun registerClicks() {
-
-        binding.relAddAddress.setOnClickListener {
-            curActivity().move2EditAddressFragment(null)
-        }
-
-    }
+	private fun searchFunctionality() {
+		binding.edtSearchAddress.addTextChangedListener {
+			if (it == null) return@addTextChangedListener
+			addressViewModel.searchAddressByName(name = it.toString().trim())
+		}
+	}
 
 
-    private fun curActivity() = requireActivity() as MainActivity
+	private fun initRecAddressView() {
+		addressAdapter = AddressAdapter(this, anim = anim, shouldBeClickable)
+		binding.recViewAddresses.apply {
+			layoutManager = LinearLayoutManager(curActivity())
+			adapter = addressAdapter
+		}
+		lifecycleScope.launchWhenStarted {
+			addressViewModel.address_items.collect {
+				it?.let {
+					VLog.d("Getting List of Addresses : $it")
+					addressAdapter!!.updateAddressList(it)
+				}
+			}
+		}
+	}
 
-    override fun onDelete(itemAddress: Address) {
-        curActivity().apply {
-            dialogManager.showFailureDialog(
-                this,
-                getStringResource(R.string.address_book_pop_up_delete_title),
-                getStringResource(R.string.address_book_pop_up_delete_description),
-                getStringResource(R.string.confirm_btn)
-            ) {
-                addressViewModel.deleteAddressItem(itemAddress)
-                curActivity().apply {
-                    dialogManager.showSuccessDialog(
-                        this,
-                        getStringResource(R.string.adress_book_pop_up_removed_title),
-                        getStringResource(R.string.adress_book_pop_up_removed_description),
-                        getStringResource(R.string.ready_btn)
-                    ) {
-                        
-                    }
-                }
-            }
-        }
-    }
+	private fun registerClicks() {
 
-    override fun onEdit(itemAddress: Address) {
-        curActivity().move2EditAddressFragment(itemAddress)
-    }
+		binding.relAddAddress.setOnClickListener {
+			curActivity().move2EditAddressFragment(null)
+		}
 
-    override fun onSend(itemAddress: Address) {
-        val intent = Intent(Intent.ACTION_SEND)
-        val content = itemAddress.address
-        intent.type = "text/plain"
-        intent.putExtra(
-            Intent.EXTRA_SUBJECT,
-            getString(R.string.address_wallet)
-        )
-        intent.putExtra(Intent.EXTRA_TEXT, content)
-        requestSendingTextViaMessengers.launch(intent)
+	}
 
-    }
 
-    override fun onAddressClicked(address: String) {
-        VLog.d("Saving address to send money : $address")
-        curActivity().mainViewModel.saveChosenAddress(address)
-        curActivity().popBackStackOnce()
-    }
+	private fun curActivity() = requireActivity() as MainActivity
 
-    private val requestSendingTextViaMessengers =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+	override fun onDelete(itemAddress: Address) {
+		curActivity().apply {
+			dialogManager.showFailureDialog(
+				this,
+				getStringResource(R.string.address_book_pop_up_delete_title),
+				getStringResource(R.string.address_book_pop_up_delete_description),
+				getStringResource(R.string.confirm_btn)
+			) {
+				curActivity().apply {
+					dialogManager.showSuccessDialog(
+						this,
+						getStringResource(R.string.adress_book_pop_up_removed_title),
+						getStringResource(R.string.adress_book_pop_up_removed_description),
+						getStringResource(R.string.ready_btn)
+					) {
+						addressViewModel.deleteAddressItem(itemAddress)
+					}
+				}
+			}
+		}
+	}
 
-        }
+	override fun onEdit(itemAddress: Address) {
+		curActivity().move2EditAddressFragment(itemAddress)
+	}
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-    }
+	override fun onSend(itemAddress: Address) {
+		val intent = Intent(Intent.ACTION_SEND)
+		val content = itemAddress.address
+		intent.type = "text/plain"
+		intent.putExtra(
+			Intent.EXTRA_SUBJECT,
+			getString(R.string.address_wallet)
+		)
+		intent.putExtra(Intent.EXTRA_TEXT, content)
+		requestSendingTextViaMessengers.launch(intent)
+
+	}
+
+	override fun onAddressClicked(address: String) {
+		VLog.d("Saving address to send money : $address")
+		curActivity().mainViewModel.saveChosenAddress(address)
+		curActivity().popBackStackOnce()
+	}
+
+	private val requestSendingTextViaMessengers =
+		registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+
+		}
+
+	override fun onDestroyView() {
+		super.onDestroyView()
+	}
 
 
 }
--- app/src/main/java/com/android/greenapp/presentation/main/address/AddressViewModel.kt
+++ app/src/main/java/com/android/greenapp/presentation/main/address/AddressViewModel.kt
@@ -6,6 +6,7 @@ import com.android.greenapp.domain.entity.Address
 import com.android.greenapp.domain.interact.AddressInteract
 import com.example.common.tools.VLog
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collect
@@ -17,59 +18,67 @@ import javax.inject.Inject
  * email: bekjan.omirzak98@gmail.com
  */
 class AddressViewModel @Inject constructor(private val addressInteract: AddressInteract) :
-    ViewModel() {
+	ViewModel() {
 
-    private val _address_items = MutableStateFlow<List<Address>?>(null)
-    val address_items: Flow<List<Address>?> = _address_items
-    private var job: Job? = null
+	private val _address_items = MutableStateFlow<List<Address>?>(null)
+	val address_items: Flow<List<Address>?> = _address_items
+	private var job: Job? = null
 
 
-    init {
-        getAddressList()
-    }
+	init {
+		getAddressList()
+	}
 
-    private fun getAddressList() {
-        job?.cancel()
-        job = viewModelScope.launch {
-            try {
-                val flowOfAddress = addressInteract.getAllSavedAddressList()
-                flowOfAddress.collect {
-                    _address_items.emit(it)
-                }
-            } catch (ex: Exception) {
-                VLog.e("Exception in getting flowAddressList : $ex")
-            }
-        }
-    }
+	private fun getAddressList() {
+		job?.cancel()
+		job = viewModelScope.launch {
+			try {
+				val flowOfAddress = addressInteract.getAllSavedAddressList()
+				flowOfAddress.collect {
+					val sorted = it.sortedWith(object : Comparator<Address> {
+						override fun compare(p0: Address, p1: Address): Int {
+							if (p0.updated_time < p1.updated_time)
+								return -1
+							return 1
+						}
+					})
+					_address_items.emit(sorted)
+				}
+			} catch (ex: Exception) {
+				VLog.e("Exception in getting flowAddressList : $ex")
+			}
+		}
+	}
 
-    fun updateAddressItem(address: Address) {
-        viewModelScope.launch {
-            addressInteract.updateAddressEntity(address)
-        }
-    }
+	fun updateAddressItem(address: Address) {
+		viewModelScope.launch {
+			addressInteract.updateAddressEntity(address)
+		}
+	}
 
-    fun searchAddressByName(name: String) {
-        viewModelScope.launch {
-            val res = addressInteract.searchAddressByName(name)
-            res.collect {
-                _address_items.emit(it)
-            }
-        }
-    }
+	fun searchAddressByName(name: String) {
+		viewModelScope.launch {
+			val res = addressInteract.searchAddressByName(name)
+			res.collect {
+				_address_items.emit(it)
+			}
+		}
+	}
 
-    fun insertAddressItem(address: Address) {
-        viewModelScope.launch {
-            addressInteract.insertAddressEntity(address)
-        }
-    }
+	fun insertAddressItem(address: Address) {
+		viewModelScope.launch {
+			addressInteract.insertAddressEntity(address)
+		}
+	}
 
-    fun deleteAddressItem(address: Address) {
-        viewModelScope.launch {
-            addressInteract.deleteAddressEntity(address)
-        }
-    }
+	fun deleteAddressItem(address: Address) {
+		viewModelScope.launch {
+			delay(500)
+			addressInteract.deleteAddressEntity(address)
+		}
+	}
 
-    suspend fun checkIfAddressExistInDb(address: String) =
-        addressInteract.checkIfAddressAlreadyExist(address)
+	suspend fun checkIfAddressExistInDb(address: String) =
+		addressInteract.checkIfAddressAlreadyExist(address)
 
 }
