fd99dc4a67cf9631aa7eff692f4371e83ed97b49
user <mail@mail.com>
2023-04-07 17:13:28 +0600

    dealing with nav controller

---
+++ app/src/main/java/com/green/wallet/presentation/main/swap/exchange/ExchangeViewModel.kt
@@ -0,0 +1,10 @@
+package com.green.wallet.presentation.main.swap.exchange
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class ExchangeViewModel @Inject constructor() : ViewModel() {
+
+
+
+}
