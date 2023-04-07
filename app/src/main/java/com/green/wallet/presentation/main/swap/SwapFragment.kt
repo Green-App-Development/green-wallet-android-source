3db635e07b4eb2785ccf466f2458a868e9062eec
user <mail@mail.com>
2023-04-07 12:57:16 +0600

    before starting swap

---
+++ app/src/main/java/com/green/wallet/presentation/main/swap/SwapFragment.kt
@@ -0,0 +1,29 @@
+package com.green.wallet.presentation.main.swap
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dagger.android.support.DaggerFragment
+
+class SwapFragment : DaggerFragment() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+	}
+
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		return super.onCreateView(inflater, container, savedInstanceState)
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+
+	}
+
+
+}
