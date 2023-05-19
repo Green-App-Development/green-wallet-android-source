8da0edf43d14dffec99ff0342cf8ee31d9624c2d
user <mail@mail.com>
2023-05-19 17:56:18 +0600

    dealing with data base migration

--- app/src/main/java/com/green/wallet/data/local/AppDatabase.kt
+++ app/src/main/java/com/green/wallet/data/local/AppDatabase.kt
@@ -1,8 +1,13 @@
 package com.green.wallet.data.local
 
+import androidx.room.AutoMigration
 import androidx.room.Database
+import androidx.room.RenameColumn
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.AutoMigrationSpec
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.green.wallet.data.local.entity.*
 
 @Database(
@@ -13,7 +18,10 @@ import com.green.wallet.data.local.entity.*
 		NFTInfoEntity::class, NFTCoinEntity::class
 	],
 	version = 34,
-	exportSchema = false
+	exportSchema = true,
+	autoMigrations = [
+		AutoMigration(from = 25, to = 34)
+	]
 )
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
@@ -26,11 +34,74 @@ abstract class AppDatabase : RoomDatabase() {
 	abstract val spentCoinsDao: SpentCoinsDao
 	abstract val faqDao: FAQDao
 	abstract val nftCoinsDao: NftCoinsDao
-	abstract val nftInfoDao:NftInfoDao
+	abstract val nftInfoDao: NftInfoDao
+
 
 	companion object {
-		const val APP_DB_NAME = "green_app_database_name"
-	}
 
+		const val APP_DB_NAME = "green_app_database_name"
+
+		val migration25To34 = object : Migration(25, 34) {
+
+			override fun migrate(database: SupportSQLiteDatabase) {
+
+				database.execSQL(
+					"CREATE TABLE IF NOT EXISTS `NFTInfoEntity` (" +
+							"`nft_coin_hash` TEXT NOT NULL, " +
+							"`nft_id` TEXT NOT NULL, " +
+							"`launcher_id` TEXT NOT NULL, " +
+							"`owner_did` TEXT NOT NULL, " +
+							"`minter_did` TEXT NOT NULL, " +
+							"`royalty_percentage` INTEGER NOT NULL, " +
+							"`mint_height` INTEGER NOT NULL, " +
+							"`data_url` TEXT NOT NULL, " +
+							"`data_hash` TEXT NOT NULL, " +
+							"`meta_hash` TEXT NOT NULL, " +
+							"`meta_url` TEXT NOT NULL, " +
+							"`description` TEXT NOT NULL, " +
+							"`collection` TEXT NOT NULL, " +
+							"`properties` TEXT NOT NULL, " +
+							"`name` TEXT NOT NULL, " +
+							"`address_fk` TEXT NOT NULL, " +
+							"`spent` INTEGER NOT NULL, " +
+							"PRIMARY KEY(`nft_coin_hash`), " +
+							"FOREIGN KEY(`address_fk`) REFERENCES `WalletEntity`(`address`) ON DELETE CASCADE)"
+				)
+
+				database.execSQL(
+					"CREATE TABLE IF NOT EXISTS `NFTCoinEntity` (" +
+							"`coin_info` TEXT NOT NULL, " +
+							"`address_fk` TEXT NOT NULL, " +
+							"`coin_hash` TEXT NOT NULL, " +
+							"`amount` INTEGER NOT NULL, " +
+							"`confirmed_block_index` INTEGER NOT NULL, " +
+							"`spent_block_index` INTEGER NOT NULL, " +
+							"`time_stamp` INTEGER NOT NULL, " +
+							"`puzzle_hash` TEXT NOT NULL, " +
+							"PRIMARY KEY(`coin_info`), " +
+							"FOREIGN KEY(`address_fk`) REFERENCES `WalletEntity`(`address`) ON DELETE CASCADE)"
+				)
+
+				database.execSQL(
+					"CREATE TABLE IF NOT EXISTS `TransactionEntity` (" +
+							"`transaction_id` TEXT NOT NULL, " +
+							"`amount` REAL NOT NULL, " +
+							"`created_at_time` INTEGER NOT NULL, " +
+							"`height` INTEGER NOT NULL, " +
+							"`status` TEXT NOT NULL, " +
+							"`networkType` TEXT NOT NULL, " +
+							"`to_dest_hash` TEXT NOT NULL, " +
+							"`fkAddress` TEXT NOT NULL, " +
+							"`fee_amount` REAL NOT NULL, " +
+							"`code` TEXT NOT NULL, " +
+							"`confirm_height` INTEGER NOT NULL DEFAULT 0, " +
+							"`nft_coin_hash` TEXT NOT NULL DEFAULT '', " +
+							"PRIMARY KEY(`transaction_id`), " +
+							"FOREIGN KEY(`fkAddress`) REFERENCES `WalletEntity`(`address`) ON DELETE CASCADE)"
+				)
+			}
+
+		}
+	}
 
 }
--- app/src/main/java/com/green/wallet/data/local/entity/TransactionEntity.kt
+++ app/src/main/java/com/green/wallet/data/local/entity/TransactionEntity.kt
@@ -9,57 +9,57 @@ import com.green.wallet.presentation.tools.Status
 
 
 @Entity(
-    tableName = "TransactionEntity",
-    foreignKeys = [
-        ForeignKey(
-            entity = WalletEntity::class,
-            parentColumns = ["address"],
-            childColumns = ["fkAddress"],
-            onDelete = ForeignKey.CASCADE
-        )
-    ]
+	tableName = "TransactionEntity",
+	foreignKeys = [
+		ForeignKey(
+			entity = WalletEntity::class,
+			parentColumns = ["address"],
+			childColumns = ["fkAddress"],
+			onDelete = ForeignKey.CASCADE
+		)
+	]
 )
 data class TransactionEntity(
-    @ColumnInfo(name = "transaction_id")
-    @PrimaryKey(autoGenerate = false)
-    var transaction_id: String,
-    @ColumnInfo(name = "amount")
-    var amount: Double,
-    @ColumnInfo(name = "created_at_time")
-    val created_at_time: Long,
-    @ColumnInfo(name = "height")
-    val height: Long,
-    @ColumnInfo(name = "status")
-    val status: Status,
-    @ColumnInfo(name = "network_type")
-    val networkType: String,
-    @ColumnInfo(name = "to_dest_hash")
-    val to_dest_hash: String,
-    @ColumnInfo(name = "fkAddress")
-    val fkAddress: String,
-    @ColumnInfo(name = "fee_amount")
-    val fee_amount: Double,
-    @ColumnInfo(name = "code")
-    val code: String,
-    @ColumnInfo(name = "confirm_height")
-    val confirm_height: Int,
-    @ColumnInfo(name = "nft_coin_hash")
-    val nft_coin_hash: String = ""
+	@ColumnInfo(name = "transaction_id")
+	@PrimaryKey(autoGenerate = false)
+	var transaction_id: String,
+	@ColumnInfo(name = "amount")
+	var amount: Double,
+	@ColumnInfo(name = "created_at_time")
+	val created_at_time: Long,
+	@ColumnInfo(name = "height")
+	val height: Long,
+	@ColumnInfo(name = "status")
+	val status: Status,
+	@ColumnInfo(name = "network_type")
+	val networkType: String,
+	@ColumnInfo(name = "to_dest_hash")
+	val to_dest_hash: String,
+	@ColumnInfo(name = "fkAddress")
+	val fkAddress: String,
+	@ColumnInfo(name = "fee_amount")
+	val fee_amount: Double,
+	@ColumnInfo(name = "code")
+	val code: String,
+	@ColumnInfo(name = "confirm_height", defaultValue = "0")
+	val confirm_height: Int,
+	@ColumnInfo(name = "nft_coin_hash", defaultValue = "")
+	val nft_coin_hash: String = ""
 ) {
 
-    fun toTransaction(localTime: Long) =
-        Transaction(
-            transaction_id,
-            amount,
-            localTime,
-            height,
-            status,
-            networkType,
-            to_dest_hash,
-            fkAddress,
-            fee_amount,
-            code,
+	fun toTransaction(localTime: Long) =
+		Transaction(
+			transaction_id,
+			amount,
+			localTime,
+			height,
+			status,
+			networkType,
+			to_dest_hash,
+			fkAddress,
+			fee_amount,
+			code,
 			nft_coin_hash
-        )
+		)
 
 }
