76dbf19e6693ed14f573e88aac33d3412764b15d
user <mail@mail.com>
2023-05-22 15:54:58 +0600

    testing migration

--- app/src/main/java/com/green/wallet/data/local/entity/AddressEntity.kt
+++ app/src/main/java/com/green/wallet/data/local/entity/AddressEntity.kt
@@ -7,20 +7,29 @@ import com.green.wallet.domain.domainmodel.Address
 
 @Entity(tableName = "AddressEntity")
 data class AddressEntity(
-    @PrimaryKey(autoGenerate = false)
-    @ColumnInfo(name = "address_id")
-    val address_id: String,
-    @ColumnInfo(name = "address")
-    val address: String,
-    @ColumnInfo(name = "name")
-    val name: String,
-    @ColumnInfo(name = "description")
-    val description: String,
-    @ColumnInfo(name="updated_time")
-    val updated_time:Long
+	@PrimaryKey(autoGenerate = false)
+	@ColumnInfo(name = "address_id")
+	val address_id: String,
+	@ColumnInfo(name = "address")
+	val address: String,
+	@ColumnInfo(name = "name")
+	val name: String,
+	@ColumnInfo(name = "description")
+	val description: String,
+	@ColumnInfo(name = "updated_time")
+	val updated_time: Long,
+	@ColumnInfo(name = "temp", defaultValue = "")
+	val temp: String=""
 ) {
 
-    fun toAddress() =
-        Address(address_id,address = address, name = name, description = description,updated_time=updated_time)
+	fun toAddress() =
+		Address(
+			address_id,
+			address = address,
+			name = name,
+			description = description,
+			updated_time = updated_time
+		)
+
 
 }
