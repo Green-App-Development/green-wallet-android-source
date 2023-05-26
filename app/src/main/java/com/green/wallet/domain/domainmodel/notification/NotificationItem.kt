9c3b0515a36b68525f436211dcd2c50684c86aef
user <mail@mail.com>
2023-05-26 12:47:54 +0600

    done witn on notif

--- app/src/main/java/com/green/wallet/domain/domainmodel/notification/NotificationItem.kt
+++ app/src/main/java/com/green/wallet/domain/domainmodel/notification/NotificationItem.kt
@@ -4,13 +4,14 @@ import com.green.wallet.presentation.tools.Status
 
 
 data class NotificationItem(
-    val status: Status = Status.Incoming,
-    val amount: Double=0.0,
-    val amountInUSD: Double=0.0,
-    val networkType: String="",
-    val height: Long=0L,
-    val commission: Double=0.0,
-    val created_at_time: Long=0L,
-    val message: String="",
-	val code:String=""
+	val status: Status = Status.Incoming,
+	val amount: Double = 0.0,
+	val amountInUSD: Double = 0.0,
+	val networkType: String = "",
+	val height: Long = 0L,
+	val commission: Double = 0.0,
+	val created_at_time: Long = 0L,
+	val message: String = "",
+	val code: String = "",
+	val nft_coin_hash: String = ""
 )
