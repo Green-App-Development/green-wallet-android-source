58a4f3f6c1934ec4b20b3476869bcdae43cd676c
user <mail@mail.com>
2023-06-02 12:49:37 +0600

    checking migratiion

--- app/src/main/java/com/green/wallet/data/local/AppDatabase.kt
+++ app/src/main/java/com/green/wallet/data/local/AppDatabase.kt
@@ -21,8 +21,7 @@ import com.green.wallet.data.local.entity.*
 	version = 35,
 	exportSchema = true,
 	autoMigrations = [
-		AutoMigration(from = 25, to = 34),
-		AutoMigration(from = 34, to = 35)
+		AutoMigration(from = 25, to = 34)
 	]
 )
 @TypeConverters(Converters::class)
@@ -115,8 +114,6 @@ abstract class AppDatabase : RoomDatabase() {
         """.trimIndent()
 				database.execSQL(createTableQuery)
 			}
-
-
 
 		}
 
